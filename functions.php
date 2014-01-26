2e913121506663b774354d59ab464eb981d338ba
Iconoblast <salvatore.the.candela@gmail.com>
2014-01-25 21:02:59 -0800

    I take it back.

--- functions.php
+++ functions.php
@@ -102,7 +102,6 @@ function _stilts_scripts() {
 
 	wp_enqueue_script( '_stilts-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
-  wp_register_script('jquery-off-canvas', get_template_directory_uri() . '/js/page-off-canvas.js', array('jquery'));)
 	
   if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
